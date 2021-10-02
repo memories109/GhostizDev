25e443e6cf151c8ac948ddcf519bdd8c2bd66548
사용자이름 <>
2021-10-02 12:37:13 +0900

    sd

--- Step01/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Step01/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,21 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	//testestse
+	
 
-	@GetMapping("/")
-	public String root(){
-		return String.format("Hello!! Spring Boot");
-	}
+	
 
 }
